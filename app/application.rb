37025a626d7047c68b64d0a0ebf646d08a47d071
Joseph Parker <jparker010190@gmail.com>
2018-05-06 03:35:24 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,9 @@
 class Application
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    num_1 = Kernel.rand(1..20)
+    num_2 = Kernel.rand(1..20)
+    num_3 = Kernel.rand(1..20)
     resp.finish
   end
 
